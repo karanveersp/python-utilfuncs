8abd35c69507a308c7fe80c26201e21af0b43aec
Karanveer <karan_4496@hotmail.com>
2020-02-25 15:21:15 -0500

    Created readme and updated setup.py

---
+++ README.md
@@ -0,0 +1,11 @@
+## Python Utility Library
+
+Import the library:
+    `import utilfuncs as util`
+    
+Use the library:
+    `util.get_rows(csvpath, skip_header=True)`
+    
+#### Contrubutors
+- Karanveer Plaha
+- Manisha Mahender
--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='utilfuncs',
     version='20.2.25',
-    description='Utility functions for general use',
+    author="Karanveer Plaha",
+    description="Simple utility functions for general use.",
     packages=find_packages(exclude=['tests']),
     install_requires=[
         'pdoc3',
         'pipreqs',
         'pytest',
-        'pytest-mock',
     ]
 )
